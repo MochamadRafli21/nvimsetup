6b4039edeeea6e8e5fe8f7f2ab3eef0092ed5333
rafli <rafliforbusiness@gmail.com>
2023-04-22 03:48:02 +0700

    init

---
+++ lua/uniso/remap.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", "{", "{}<ESC>i")
+vim.keymap.set("i", "(", "()<ESC>i")
+vim.keymap.set("i", "[", "[]<ESC>i")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
